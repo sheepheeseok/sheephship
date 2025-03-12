71f14789a9a6fcd00350ee3eff85276c63f5bc5e
heeseok <heeseokid@g.shingu.ac.kr>
2025-03-12 21:28:14 +0900

    Home 완성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx"
 import Navbar from "./component/Navbar.jsx";
 import Footer from "./component/Footer.jsx";
 
@@ -9,6 +10,7 @@ function App() {
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>} /> {/* 홈 */}
+                <Route path="/Login" element={<Login/>} /> {/* 로그인 */}
             </Routes>
             <Footer/>
         </>
--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,6 +1,22 @@
 const Footer = () => {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="footer-link">
+                <a>이용약관</a>
+                <a>개인정보처리방침</a>
+                <a>사업자정보확인</a>
+            </div>
+
+            <div className="footer-info">
+                <h1>상호: 쉽슆웍스 (SHEEPSHIPWORKS) | 대표: 양희석 | 개인정보관리책임자: 홍준화 | 전화 070-1234-5678 | 이메일: info@sheep.com</h1>
+                <h1>주소: 성남시 분당구 신구로18길 57, 타운빌딩 6층 | 사업자등록번호: 847-63-00063 | 통신판매: 2020-908432-0860 | 호스팅제공자: (주)클밍샵</h1>
+            </div>
+
+            <div className="footer-partner">
+                <img src="/icons/toss.svg" alt="toss" className="toss"/>
+                <h1>안전구매(에스크로) 서비스 가맹점</h1>
+            </div>
+        </div>
     )
 }
 
