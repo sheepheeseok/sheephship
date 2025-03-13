0b5fa0d372b4f9a2cc2d644eecfca4caf3035603
heeseokid <heeseokid@g.shingu.ac.kr>
2025-03-13 15:14:45 +0900

    Login 페이지 제작중

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,15 +2,14 @@ const Login = () => {
     return (
         <div className="container">
             <div className="login-container">
-                <div className="login-imgbox">
                     <img src="/imgs/login_logo.png" alt="login_logo" className="login_logo"/>
-                </div>
-
                 <div className="login-box">
-                    <h1>ID</h1>
-                    <div className="id-box">
-
+                    <h1>Email</h1>
+                    <div className="email-box">
+                        <img src="/icons/email.svg" alt="email" className="email-icon"/>
                     </div>
+                    <h2>Password</h2>
+                    <div className="email-box"></div>
                 </div>
             </div>
         </div>
