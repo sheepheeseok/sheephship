7416a4e27e78c2bbb53835ee341226213b008d49
dytpq <suhokym@gmail.com>
2025-03-13 23:43:01 +0900

    설정테스트완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,29 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx"
 import Navbar from "./component/Navbar.jsx";
 import Footer from "./component/Footer.jsx";
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+// import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
 function App() {
-    const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-    }, []);
+    // const [hello, setHello] = useState('')
+    //
+    // useEffect(() => {
+    //     axios.get('/api/hello')
+    //         .then(response => setHello(response.data))
+    //         .catch(error => console.log(error))
+    // }, []);
 
     return(
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-        // <>
-        //     <Navbar/>
-        //     <Routes>
-        //         <Route path="/" element={<Home/>} /> {/* 홈 */}
-        //         <Route path="/Login" element={<Login/>} /> {/* 로그인 */}
-        //     </Routes>
-        //     <Footer/>
-        // </>
+        // <div>
+        //     백엔드에서 가져온 데이터입니다 : {hello}
+        // </div>
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>} /> {/* 홈 */}
+                <Route path="/Login" element={<Login/>} /> {/* 로그인 */}
+            </Routes>
+            <Footer/>
+        </>
     )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:8080',  // Spring 서버 주소로 프록시 설정
     },
   },
-};
+});
