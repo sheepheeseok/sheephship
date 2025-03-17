8a763346020a4c24684c3fde989cb9d08d9cf682
sheepheeseok <heeseokid@g.shingu.ac.kr>
2025-03-17 17:19:23 +0900

    Product 상세 페이지 제작 중

--- frontend/src/component/FloatingButton.jsx
+++ frontend/src/component/FloatingButton.jsx
@@ -7,11 +7,15 @@ const FloatingButton = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleQuestionClick = (e) => {
+        e.stopPropagation(); // 이벤트 전파 방지
+    };
+
     return (
         <div className="floating-box" onClick={handleClick}>
             <div className="icon-container">
                 {/* Question 아이콘은 fv-icon 왼쪽에 따로 배치 */}
-                <img src="/icons/question-icon.svg" alt="question-icon" className="question-icon" />
+                <img src="/icons/question-icon.svg" alt="question-icon" className="question-icon" onClick={handleQuestionClick}/>
             </div>
             <div className={`v-icon ${isOpen ? "open" : ""}`}>
                 <img src="/icons/f-icon.svg" alt="ficon" className={`fv-icon ${isOpen ? "open" : ""}`} />
