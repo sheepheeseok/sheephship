3df622b45d9e07004b469cdb1394ea81066ca267
sheepheeseok <heeseokid@g.shingu.ac.kr>
2025-03-18 16:40:14 +0900

    제품 상세 페이지 제작 완료

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 
 const Product = () => {
+    {/* 제품 개수 카운팅 버튼 */}
     const [quantity, setQuantity] = useState(1);
-
     const increaseQuantity = () => {
         setQuantity(quantity + 1);
     };
-
     const decreaseQuantity = () => {
         if (quantity > 1) {
             setQuantity(quantity - 1);
         }
     };
 
+    {/* 제품 정보 더보기 */}
+    const [isExpanded, setIsExpanded] = useState(false);
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    {/* 제품 메인 로고 변경 */}
+    const [currentImage, setCurrentImage] = useState("/imgs/product1.png");
+    const changeImage = (imageSrc) => {
+        setCurrentImage(imageSrc);
+    };
+
+
     return (
         <div className="container">
             <div className="Product-container">
-            <img src="/imgs/Productimg.png" alt="Product-img" className="Product-img"/>
+                <img src={currentImage} alt="Product-img" className="Product-img"/>
 
                 <div className="Product-box">
                     <h1>쉽쉽 메탈립 초크백 그레이 (grey)</h1>
@@ -66,14 +78,14 @@ const Product = () => {
                     </div>
 
                     <div className="product-btn-box">
-                    <button className="buy-btn">구매하기</button>
-                    <button className="cart-btn">장바구니</button>
+                        <button className="buy-btn">구매하기</button>
+                        <button className="product-cart-btn">장바구니</button>
                     </div>
 
                     <div className="product-toss-box">
                         <div className="product-toss-text">
-                        <h1>쉽고 빠른</h1>
-                        <h1>토스페이 간편결제</h1>
+                            <h1>쉽고 빠른</h1>
+                            <h1>토스페이 간편결제</h1>
                         </div>
                         <button className="product-toss-btn">
                             <img src="/icons/toss-pay-logo.png.svg" alt="toss-pay-logo" className="toss-pay-logo"/>
@@ -81,6 +93,59 @@ const Product = () => {
                         </button>
                     </div>
                 </div>
+            </div>
+            <div className="product-img-info">
+                <img src="/imgs/product1.png" alt="product1" className="info-product-img" onClick={() => changeImage("/imgs/product1.png")} onMouseEnter={() => handleImageHover("/imgs/product1.png")}/>
+                <img src="/imgs/product2.png" alt="product2" className="info-product-img" onClick={() => changeImage("/imgs/product2.png")} onMouseEnter={() => handleImageHover("/imgs/product1.png")}/>
+                <img src="/imgs/product3.png" alt="product3" className="info-product-img" onClick={() => changeImage("/imgs/product3.png")} onMouseEnter={() => handleImageHover("/imgs/product1.png")}/>
+                <img src="/imgs/product4.png" alt="product4" className="info-product-img" onClick={() => changeImage("/imgs/product4.png")} onMouseEnter={() => handleImageHover("/imgs/product1.png")}/>
+            </div>
+
+            <div className="product-info-title">
+                <a>상품 설명</a>
+                <a>후기 (0)</a>
+                <a>Q&N</a>
+                <a>관련 상품</a>
+            </div>
+
+            <div className="product-info-box">
+                <li>
+                    소재:겉감 - 100% nylon
+                    <span className="indent">안감 - 100% polyester (micro fleece)</span>
+                </li>
+                <li>크기 : 20 X 15 cm (가로X높이)</li>
+                <li>안감색상 : light grey</li>
+                <li>제조국 : 한국</li>
+                <li>제조사 : 오름 (ORUMM)</li>
+
+                <button onClick={toggleExpand} className="expand-btn">
+                    {isExpanded ? '상품정보 접기' : '상품정보 더보기'}
+                    <img
+                        src="/icons/p-info-icon.svg"
+                        alt="p-info-icon"
+                        style={{
+                            marginTop: "2px",
+                            transition: 'transform 0.3s ease',  // 회전 효과 추가
+                            transform: isExpanded ? 'rotate(180deg)' : 'rotate(0deg)'  // isExpanded에 따라 회전
+                        }}
+                    />
+                </button>
+
+                <div className="image-container" style={{maxHeight: isExpanded ? 'none' : '400px'}}>
+                    <img src="/imgs/product1.png" alt="p-detail-img" className="p-detail-img"
+                         style={{marginTop: "96px"}}/>
+                    <h1>DETAILS</h1>
+                    <img src="/imgs/p-detail-img2.png" alt="p-detail-img" className="p-detail-img"/>
+                    <img src="/imgs/p-detail-img3.png" alt="p-detail-img" className="p-detail-img"
+                         style={{marginTop: "64px"}}/>
+                    <img src="/imgs/p-detail-img4.png" alt="p-detail-img" className="p-detail-img"
+                         style={{marginTop: "64px"}}/>
+                    <h2 style={{marginTop: "100px"}}>*상품의 색상은 모니터 해상도에 따라 실제 색상과 다소 차이가 있을 수 있으며 구매 전 상세 사진을 꼼꼼히 확인
+                        바랍니다.</h2>
+                    <img src="/imgs/p-detail-img5.png" alt="p-detail-img" className="p-detail-img"
+                         style={{marginTop: "5px"}}/>
+                </div>
+
             </div>
         </div>
     )
