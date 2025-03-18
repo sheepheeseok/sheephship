017bf4b372ab787888277b991b94b97752b21cbe
suhokym <suhokym@gmail.com>
2025-03-18 16:41:57 +0900

    Reviews 수정

--- backend/src/main/java/sheepback/domain/Reviews.java
+++ backend/src/main/java/sheepback/domain/Reviews.java
@@ -1,8 +1,19 @@
 package sheepback.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Reviews {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "review_id")
+    private Long id;
+
 
 }
