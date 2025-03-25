35c41cf0160873a72aeed829c65dc318c2fad7b3
dytpq <suhokym@gmail.com>
2025-03-25 23:43:28 +0900

    완전수정후 내일 할것 ItemCategoryRepository, ItemRepository에 저장헀음

--- backend/src/main/java/sheepback/controller/MemberApiController.java
+++ backend/src/main/java/sheepback/controller/MemberApiController.java
@@ -107,8 +107,8 @@ public class MemberApiController {
         return memberDtos;
     }
 
-    //회원정보 수정시 멤버정보 가져오기
-    @PostMapping("/api/updateMemberInfo")
+    //마이페이지 이동시 가져오는 데이터
+    @PostMapping("/api/myPageInfo")
     public MyPageDto getById(@CookieValue(name = "loginId", required = false) String id) {
 
         //id반환 실패시 null반환
@@ -125,6 +125,25 @@ public class MemberApiController {
 
     }
 
+    @PostMapping("/api/updateForm")
+    public UpdateForm findById(@CookieValue(name = "loginId", required = false) String id) {
+
+        //id반환 실패시 null반환
+        if (id == null) {
+            return null;
+        }
+
+        Member member = memberService.getMemberById(id);
+        UpdateForm updateForm = new UpdateForm(member);
+
+
+
+        return updateForm;
+
+    }
+
+
+
 
     //비밀번호 변경
     @PostMapping("/api/changePassword")
@@ -137,7 +156,8 @@ public class MemberApiController {
     //회원 정보 업데이트
     @PostMapping("/api/updateMember")
     public UpdateMemberDto updateMember(@CookieValue(name = "loginId", required = false) String id,
-                                        @RequestBody @Valid UpdateMember updatemember) {
+                                        @RequestBody @Valid UpdateMember updatemember,
+                                        HttpServletResponse response) {
         Member member = memberService.updateMember(id,
                 updatemember.getPassword(),
                 updatemember.getName(),
@@ -147,21 +167,21 @@ public class MemberApiController {
                 updatemember.isAgreeAge(),
                 updatemember.isAgreeMarketing());
 
-        UpdateMemberDto newupdateMember = new UpdateMemberDto();
-        newupdateMember.setId(member.getId());
-        newupdateMember.setName(member.getName());
-        newupdateMember.setGrade(member.getGrade());
-        newupdateMember.setPoint(member.getPoint());
+        UpdateMemberDto newupdateMember = new UpdateMemberDto(member);
         return  newupdateMember;
     }
 
     @Data
     private static class UpdateMemberDto{
-        String id;
         String name;
         Grade grade;
         Long point;
 
+        public UpdateMemberDto(Member member) {
+            this.name = member.getName();
+            this.grade = member.getGrade();
+            this.point = member.getPoint();
+        }
     }
 
     @Data
@@ -260,5 +280,28 @@ public class MemberApiController {
     private static class FindIdDto {
         String name;
         String phoneNumber;
+    }
+
+    private class UpdateForm {
+        String id;
+        String name;
+        String password;
+        Address address;
+        String phoneNumber;
+        String email;
+        boolean agreeTerms;
+        boolean agreeMarketing;
+        boolean agreeAge;
+
+        public UpdateForm(Member member) {
+            this.name = member.getName();
+            this.password = member.getPassword();
+            this.address = member.getAddress();
+            this.phoneNumber = member.getPhoneNumber();
+            this.email = member.getEmail();
+            this.agreeTerms = member.isAgreeTerms();
+            this.agreeMarketing = member.isAgreeMarketing();
+            this.agreeAge = member.isAgreeAge();
+        }
     }
 }
---
+++ backend/src/main/java/sheepback/repository/ItemCategoryRepository.java
@@ -0,0 +1,5 @@
+package sheepback.repository;
+
+public class ItemCategoryRepository {
+    // 카테고리별 상품 페이지 검색기능 만들어야함
+}
