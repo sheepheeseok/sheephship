5566446ebfd84b985ef4c07caac93a477d403f7b
Leo8078 <syt09159@g.shingu.ac.kr>
2025-04-05 01:59:43 +0900

    Navbar 드롭다운 추가

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,7 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const handleMouseEnter = () => {
+        setDropdownVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setDropdownVisible(false);
+    };
 
     return (
         <div className="navbar">
@@ -9,16 +19,27 @@ const Navbar = () => {
                 <li><a href="shop">SHOP</a></li>
                 <li><a href="#">CENTER</a></li>
                 <li><a href="#">ROOTFIND</a></li>
-                <li><a href="brand">BRAND</a></li>
+                <li
+                    className="brand-dropdown"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <a href="#">BRAND</a>
+                    <ul className={`dropdown-menu ${dropdownVisible ? 'show' : ''}`}>
+                        <li><a href="#" onClick={() => navigate("/BRAND")}>ABOUT</a></li>
+                        <li><a href="#" onClick={() => navigate("/QnA")}>Q&A</a></li>
+                        <li><a href="#" onClick={() => navigate("/faq")}>FAQ</a></li>
+                    </ul>
+                </li>
             </ul>
-            <img src="/icons/MainIcon.svg" alt="MainIcon" className="MainIcon" onClick={() => navigate("/")} style={{ cursor: "pointer" }}/>
+            <img src="/icons/MainIcon.svg" alt="MainIcon" className="MainIcon" onClick={() => navigate("/")} style={{ cursor: "pointer" }} />
             <div className="nav-icons">
-                <img src="/icons/search.svg" alt="searchIcon"/>
-                <img src="/icons/market.svg" alt="marketIcon"/>
-                <img src="/icons/profile.svg" alt="profileIcon" onClick={() => navigate("/login")} style={{ cursor: "pointer" }}/>
+                <img src="/icons/search.svg" alt="searchIcon" />
+                <img src="/icons/market.svg" alt="marketIcon" />
+                <img src="/icons/profile.svg" alt="profileIcon" onClick={() => navigate("/login")} style={{ cursor: "pointer" }} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
